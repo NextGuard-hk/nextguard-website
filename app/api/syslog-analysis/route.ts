a53639bee27cf69aebdb56430296564d8776acc5
NextGuard-hk <oscar@next-guard.com>
2026-02-27 18:02:48 +0800

    P0 security: Replace hardcoded nPoint URL in syslog-analysis

--- app/api/syslog-analysis/route.ts
+++ app/api/syslog-analysis/route.ts
@@ -3,7 +3,7 @@ import { S3Client, ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/clien
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const BUCKET = 'nextguard-downloads'
-const SYSLOG_NPOINT = 'https://api.npoint.io/3c0f8088efc551bd10d0'
+const SYSLOG_NPOINT = process.env.NPOINT_SYSLOG_URL || ''
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY || ''
 
 const S3 = new S3Client({
